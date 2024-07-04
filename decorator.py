608f1b9844d8987c6dcfde1229bcbeb1e28d990a
FineFinance <42334069+FinancialEngineerLab@users.noreply.github.com>
2024-07-05 08:35:17 +0900

    Create decorator.py

---
+++ decorator.py
@@ -0,0 +1,9 @@
+def time_fn(fn):
+    @wraps(fn)
+    def measure_time(*args, **kwargs):
+        t1 = time.time()
+        result = fn(*args, **kwargs)
+        t2 = time.time()
+        print(f"@timefn: {fn.__name__} took {t2-t1} seconds")
+        return result
+    return measure_time
